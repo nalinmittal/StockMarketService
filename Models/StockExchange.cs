1bca756bb03c79f0492185473682bdc5fd8a583e
nalinmittal <mittalnalin97@gmail.com>
2020-08-26 17:34:34 +0530

    Added models library

---
+++ Models/StockExchange.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    class StockExchange
+    {
+        public int Id { get; set; }
+        public string Stockexchange { get; set; }
+        public string Brief { get; set; }
+        public string Contactaddress { get; set; }
+        public string Remarks { get; set; }
+
+    }
+}
