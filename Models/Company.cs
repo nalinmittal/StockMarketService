85b9a0d0505369ca07d32480c14258f4a54a732c
Harsh Grover <42997856+Z3376@users.noreply.github.com>
2020-08-27 08:44:46 +0530

    Update Company.cs
    [Fix] Entity class definition

--- Models/Company.cs
+++ Models/Company.cs
@@ -2,18 +2,31 @@
 using System.Collections.Generic;
 using System.Security.Authentication.ExtendedProtection;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace Models
 {
+    [Table("company")]
     class Company
     {
+        [Key]
+        public long id { get; set; }
+        [Required]
+        [Column("cname")]
         public string Companyname { get; set; }
+        [Column("turnover")]
         public float Turnover { get; set; }
+        [Column("ceo")]
         public string Ceo { get; set; }
+        [Column("bodirectors")]
         public string Boardofdirectors { get; set; }
+        [Column("sexchanges")]
         public string Stockexchanges { get; set; }
+        [Column("sector")]
         public string Sector { get; set; }
+        [Column("brief")]
         public string Brief { get; set; }
+        [Column("scodes")]
         public string Stockcodes { get; set; }
 
     }
