b390d0a855c6f27f6e3119408290612408e6b337
Harsh Grover <grover.harsh.bitsp@gmail.com>
2020-08-29 15:17:12 +0530

    Single context class fix.

---
+++ Models/StockMarketContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    class StockMarketContext : DbContext
+    {
+        public StockMarketContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+
+        protected StockMarketContext()
+        {
+        }
+
+        public virtual DbSet<Company> Companies { get; set; }
+        public virtual DbSet<Ipodetail> Ipos { get; set; }
+        public virtual DbSet<Sector> Sectors { get; set; }
+        public virtual DbSet<StockExchange> StockExchanges { get; set; }
+        public virtual DbSet<Stockprice> Stockprices { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+    }
+}
