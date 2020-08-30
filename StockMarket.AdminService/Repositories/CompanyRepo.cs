01c940d3c41d66b3dd40b62a780d06313171c3e6
Harsh Grover <grover.harsh.bitsp@gmail.com>
2020-08-30 18:44:58 +0530

    Updated CompanyRepo

--- StockMarket.AdminService/Repositories/CompanyRepo.cs
+++ StockMarket.AdminService/Repositories/CompanyRepo.cs
@@ -1,4 +1,5 @@
-﻿using Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,29 +9,78 @@ namespace StockMarket.AdminService.Repositories
 {
     public class CompanyRepo : IRepo<Company>
     {
+        private StockMarketContext context;
+
+        public CompanyRepo(StockMarketContext context)
+        {
+            this.context = context;
+        }
+
         public bool Add(Company entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                this.context.Add(entity);
+                int updates = context.SaveChanges();
+                if(updates > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
         }
 
         public bool Delete(Company entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                this.context.Remove(entity);
+                int updates = context.SaveChanges();
+                if (updates > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
         }
 
         public IEnumerable<Company> Get()
         {
-            throw new NotImplementedException();
+            var companies = this.context.Companies;
+            return companies;
         }
 
         public Company Get(object key)
         {
-            throw new NotImplementedException();
+            var company = this.context.Companies.Find(key);
+            return company;
         }
 
         public bool Update(Company entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                this.context.Entry(entity).State = EntityState.Modified;
+                int updates = context.SaveChanges();
+                if(updates > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
         }
     }
 }
