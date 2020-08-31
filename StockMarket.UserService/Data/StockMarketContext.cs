a35176de44ee7a5bc43d06972b2a07dabf6e9db0
nalinmittal <mittalnalin97@gmail.com>
2020-08-31 22:08:05 +0530

    Updated context class

---
+++ StockMarket.UserService/Data/StockMarketContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+using Models;
+
+namespace StockMarket.UserService.Data
+{
+    
+    public class StockMarketContext : DbContext
+    {
+        public StockMarketContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+
+        protected StockMarketContext()
+        {
+        }
+
+        public virtual DbSet<Company> Companies { get; set; }
+        public virtual DbSet<Ipodetail> Ipos { get; set; }
+        public virtual DbSet<Sector> Sectors { get; set; }
+        public virtual DbSet<StockExchange> StockExchanges { get; set; }
+        public virtual DbSet<Stockprice> Stockprices { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+    }
+}
