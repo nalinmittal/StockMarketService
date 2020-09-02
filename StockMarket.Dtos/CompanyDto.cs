cd3af04a530710721067cda01c7b7d36ffcfb3be
Harsh Grover <grover.harsh.bitsp@gmail.com>
2020-09-02 14:16:08 +0530

    Added Dto

---
+++ StockMarket.Dtos/CompanyDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StockMarket.Dtos
+{
+    class CompanyDto
+    {
+        public long Id { get; set; }
+        [Required]
+        public string Companyname { get; set; }
+        [Required]
+        public float Turnover { get; set; }
+        [Required]
+        public string Ceo { get; set; }
+        [Required]
+        public string Boardofdirectors { get; set; }
+        [Required]
+        public List<long> StockExchangeIds { get; set; }
+        [Required]
+        public int SectorId { get; set; }
+        [Required]
+        public string Brief { get; set; }
+    }
+}
---
+++ StockMarket.Dtos/IpoDetailDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StockMarket.Dtos
+{
+    class IpoDetailDto
+    {
+        public long Id { get; set; }
+        [Required]
+        public int CompanyId { get; set; }
+        [Required]
+        public long StockExchangeId { get; set; }
+        [Required]
+        public float Pricepershare { get; set; }
+        [Required]
+        public int Totalnumberofshares { get; set; }
+        [Required]
+        public DateTime Opendatetime { get; set; }
+        public string Remarks { get; set; }
+    }
+}
