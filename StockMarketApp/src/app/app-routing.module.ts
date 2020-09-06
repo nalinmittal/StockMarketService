6478be7c382a9d5c53df9861dcd72a3c15e8c7b8
nalinmittal <mittalnalin97@gmail.com>
2020-09-07 04:58:12 +0530

    Setting up user UI

--- StockMarketApp/src/app/app-routing.module.ts
+++ StockMarketApp/src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './Components/Admin/landing/landing.component';
+import { UserLandingComponent } from './Components/User/user-landing/user-landing.component';
+import { UserCompanyComponent } from './Components/User/user-company/user-company.component';
+import { UserChartsComponent } from './Components/User/user-charts/user-charts.component';
+import { UserIpoComponent } from './Components/User/user-ipo/user-ipo.component';
 
 const routes: Routes = [
-  {
-    path: 'admin',
-    component: LandingComponent
-  }
+  
+    {path: 'admin',component: LandingComponent},
+    {path:'user',component:UserLandingComponent,children:[
+      {path:'company',component:UserCompanyComponent},
+      {path:'charts',component:UserChartsComponent},
+      {path:'ipo',component:UserIpoComponent}
+
+    ]}
+  
 ];
 
 @NgModule({
--- StockMarketApp/src/app/app.module.ts
+++ StockMarketApp/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './Components/Admin/landing/landing.component';
 import { CompanyComponent } from './Components/Admin/company/company.component';
+//import { SrcComponent } from './src/src.component';
+import { UserLandingComponent } from './Components/User/user-landing/user-landing.component';
+import { UserCompanyComponent } from './Components/User/user-company/user-company.component';
+import { UserIpoComponent } from './Components/User/user-ipo/user-ipo.component';
+import { UserChartsComponent } from './Components/User/user-charts/user-charts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    CompanyComponent
+    CompanyComponent,
+    //SrcComponent,
+    UserLandingComponent,
+    UserCompanyComponent,
+    UserIpoComponent,
+    UserChartsComponent
   ],
   imports: [
     BrowserModule,
